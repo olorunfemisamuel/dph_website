124324e964b76627bfea8bc41f81c640ddcfd493
Olorunfemi Samuel <forfemi@gmail.com>
2025-12-18 15:40:19 +0100

    adjusted Navbar

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ const toggleMenu = () => {
 
 <template>
  
-  <NavBar />
+ <NavBar :toggleMenu="toggleMenu" />
 
  
    
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+  defineProps<{
+  toggleMenu: () => void
+}>()
 
 </script>
 
